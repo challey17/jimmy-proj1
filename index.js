53369ecd7708bcc905d26b3c05f2b5b1861fa6b4
Chayce Knaub <chknaub@yahoo.com>
2022-07-19 22:09:26 -0400

    styles and dom stuff

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-
 //https://dog.ceo/dog-api/documentation/
 //document.getElementById("myBtn").addEventListener("click", displayDate);
 
 // EVENT LISTENERS
-const randomBtn = document.getElementById("get-random").addEventListener("click", getRandomDog)
-const searchBreedBtn = document.getElementById("get-breed").addEventListener("click", searchByBreed)
-const darkLightToggle = document.getElementById("dark-light").addEventListener("click", toggleDarkMode)
-
+document.getElementById("get-random").addEventListener("click", getRandomDog);
+document.getElementById("get-breed").addEventListener("click", searchByBreed);
+document.getElementById("dark-light").addEventListener("click", toggleDarkMode);
 
 // get random dog picture
 // https://dog.ceo/api/breeds/image/random
-function getRandomDog(){
-    
-    // get random img from api
-    fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            //display image in DOM
-            document.getElementById("random-img").src = data.message;
-        });
- }
+function getRandomDog() {
+  //clear previous image, if any
+  document.getElementById("rando").replaceChildren();
+  const randoContainer = document.getElementById("rando");
+  // get random img from api
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //display image in DOM
+      // document.getElementById("random-img").src = data.message;
+      const img = document.createElement("img");
+      img.src = data.message;
+      randoContainer.append(img);
+    });
+}
 
- // search by breed
+// search by breed
 //  https://dog.ceo/api/breed/hound/images
- function searchByBreed(){
-   // Grab input value
-   const searchInput = document.getElementById("search-input").value;
-   console.log(searchInput)
+function searchByBreed() {
+  //clear previous images, if any
+  document.getElementById("breed").replaceChildren();
+  // Grab input value
+  const searchInput = document.getElementById("search-input").value;
+  console.log(searchInput);
 
-    // get  images by breed
-    fetch(`https://dog.ceo/api/breed/${searchInput}/images`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            document.getElementById("breed-img").src = data.message[0];
-        });
- }
+  const breed = document.getElementById("breed");
 
- // toggle dark mode
- function toggleDarkMode(){
-    console.log("toggleDarkMode() runs")
- }
+  // get  images by breed
+  fetch(`https://dog.ceo/api/breed/${searchInput}/images`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message);
+      data.message.forEach((element, index) => {
+        if (index <= 2) {
+          const img = document.createElement("img");
+          img.src = element;
+          breed.append(img);
+        }
+      });
+    });
+}
+
+// toggle dark mode
+function toggleDarkMode() {
+  console.log("toggleDarkMode() runs");
+}
 
 // TO DO
 // ADD ERROR HANDLING TO SEARCH BY BREED FUNCTION
